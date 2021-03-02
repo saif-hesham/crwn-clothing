e401999d908dee108fc0e55783c84b27baedc2a7
saif-hesham <saifeldeen.hesham@gmail.com>
2021-03-02 14:54:58 +0200

    Broken down homepage into smaller components and finalized the  styles to menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './Components/homepage.component'
+import HomePage from './pages/homepage.component'
 
 function App() {
   return (
